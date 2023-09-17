6284427875093a5ab1743c82530d70437df56136
DennisBaksheev <dennis.baksheev8@gmail.com>
2023-09-17 17:38:00 -0400

    final updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
+import { getUserFragments } from './api';  // <- import added here
 
 async function init() {
   // Get our UI elements
@@ -39,6 +40,9 @@ async function init() {
 
   // Disable the Login button
   loginBtn.disabled = true;
+
+  // Call the getUserFragments function now that we have a user
+  getUserFragments(user);  // <- function call added here
 }
 
 // Wait for the DOM to be ready, then start the app
