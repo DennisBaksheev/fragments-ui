49b14447972d625395f940ff7b76ea60027ffa32
DennisBaksheev <dennis.baksheev8@gmail.com>
2023-11-13 23:57:21 -0500

    Initial commit

--- src/api.js
+++ src/api.js
@@ -1,18 +1,76 @@
-// src/api.js
-
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://54.146.213.78:8080';
-
+const apiUrl = process.env.API_URL || 'http://localhost:8080';
 
 /**
  * Given an authenticated user, request all fragments for this user from the
  * fragments microservice (currently only running locally). We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
-export async function getUserFragments(user) {
+
+// GET ALL
+export async function getUserFragments(user, expand = false) {
   console.log('Requesting user fragments data...');
+  console.log('apiUrl', apiUrl);
+  console.log('expand', expand);
+
   try {
-    const res = await fetch(`${apiUrl}/v1/fragments`, {
+    const res = await fetch(
+      `${apiUrl}/v1/fragments${expand ? '?expand=1' : ''}`,
+      {
+        // Generate headers with the proper Authorization bearer token to pass
+        headers: user.authorizationHeaders(),
+      }
+    );
+    // console.log('res', res);
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Got user fragments data', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call GET /v1/fragment', { err });
+  }
+}
+
+// GET DATA BY ID
+export async function getFragmentDataById(user, fragmentId, ext) {
+  // const fragmentId = e.target.id;
+  console.log('Getting fragment data by id...');
+  try {
+    const res = await fetch(
+      `${apiUrl}/v1/fragments/${fragmentId}${ext ? `.${ext}` : ''}`,
+      {
+        // Generate headers with the proper Authorization bearer token to pass
+        headers: user.authorizationHeaders(),
+      }
+    );
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    // const data = await res.json();
+    console.log('Got user fragments by Id res', res);
+    if (res.headers.get('Content-Type').includes('application/json')) {
+      const data = await res.json();
+      console.log('*** data', data);
+      return JSON.stringify(data);
+    } else {
+      // console.log('res.text()', await res.text());
+      return await res.text();
+    }
+  } catch (err) {
+    console.error(`Unable to call GET ${apiUrl}/v1/fragments/${fragmentId}`, {
+      err,
+    });
+  }
+}
+
+// GET FRAGMENT BY ID
+export async function getFragmentById(user, fragmentId) {
+  // const fragmentId = e.target.id;
+  console.log('Getting fragment by id info...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}/info`, {
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
     });
@@ -20,35 +78,37 @@ export async function getUserFragments(user) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log('Got user fragments data', { data });
+    console.log('JSON data:', data);
+    return data;
   } catch (err) {
-    console.error('Unable to call GET /v1/fragment', { err });
+    console.error(
+      `Unable to call GET ${apiUrl}/v1/fragments/${fragmentId}/info`,
+      {
+        err,
+      }
+    );
   }
 }
 
-/**
- * Given an authenticated user and fragment data, send a POST request to store
- * the new fragment for this user in the fragments microservice.
- */
-export async function createUserFragment(user, fragmentData) {
-  console.log('Storing new fragment for user...');
+// POST NEW FRAGMENT
+export async function submitFragment(user, fragment, dataType) {
+  console.log('Submitting fragment data...');
+  console.log('apiUrl', apiUrl);
+  console.log('dataType', dataType);
+  console.log('fragment', fragment);
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
       method: 'POST',
-      headers: {
-        ...user.authorizationHeaders(),
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(fragmentData),
+      headers: user.authorizationHeaders(dataType),
+      body: fragment,
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log('Fragment stored successfully', { data });
+    console.log('Submitted fragment data', { data });
     return data;
   } catch (err) {
-    console.error('Unable to call POST /v1/fragments', { err });
-    throw err;
+    console.error('Unable to call POST /v1/fragment', { err });
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,48 +1,41 @@
-// src/app.js
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { createUserFragment } from './api';
 import { Auth, getUser } from './auth';
-import { getUserFragments } from './api';  // <- import added here
+import {
+  getUserFragments,
+  submitFragment,
+  getFragmentDataById,
+  getFragmentById,
+  s,
+} from './api';
 
-const fragmentForm = document.querySelector('#fragmentForm');
-const fragmentText = document.querySelector('#fragmentText');
+//test onclick`
 
-fragmentForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const user = await getUser();
-    if (!user) {
-        console.error('User is not authenticated');
-        return;
-    }
-
-    const fragmentData = {
-        text: fragmentText.value,
-        // ... any other data you want to send
-    };
-
-    try {
-        await createUserFragment(user, fragmentData);
-        alert('Fragment stored successfully!');
-    } catch (err) {
-        alert('Failed to store fragment. Please try again.');
-    }
-});
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
+  const username = document.querySelector('#username');
+  const userGreeting = document.querySelector('#user-greeting');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
+  const dataSubmit = document.querySelector('#data-submit');
+  const data = document.querySelector('#data');
+  const dataTypeSelect = document.querySelector('#data-type');
+  const getData = document.querySelector('#get-data');
+  const expandCheckbox = document.querySelector('#expand');
+  // const fragmentIdList = document.querySelector('#fragment-id-list');
+  const showFragmentList = document.querySelector('.show-fragment-list');
+  // const getDetailBtn = document.querySelector('#get-detail');
+  const fragmentTable = document.querySelector('#fragment-table');
+  const fragmentTableBody = document.querySelector('#fragment-table tbody');
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
+    console.log('Logging in...');
     // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
     // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
-    Auth.federatedSignIn();
+    Auth.federatedSignIn(); // to get AWS creds directly from Cognito Federated Identities, should only call this when using OAuth flows or Hosted UI
   };
   logoutBtn.onclick = () => {
+    console.log('Logging out...');
     // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
     // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
     Auth.signOut();
@@ -56,6 +49,9 @@ async function init() {
     return;
   }
 
+  // Do an authenticated request to the fragments API server and log the result
+  let res = await getUserFragments(user);
+
   // Log the user info for debugging purposes
   console.log({ user });
 
@@ -63,14 +59,255 @@ async function init() {
   userSection.hidden = false;
 
   // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
+  userGreeting.hidden = false;
+  username.innerText = user.username;
 
   // Disable the Login button
   loginBtn.disabled = true;
 
-  // Call the getUserFragments function now that we have a user
-  getUserFragments(user);  // <- function call added here
+  // Fake JSON copy button
+  const copyBtn = document.querySelector('#copy-btn');
+  const fakeJSON = document.querySelector('#fake-json');
+  copyBtn.onclick = async () => {
+    try {
+      await navigator.clipboard.writeText(fakeJSON.innerText);
+      copyBtn.innerText = 'Copied!';
+      setTimeout(() => {
+        copyBtn.innerText = 'Copy';
+      }, 1000);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  dataSubmit.onclick = async (e) => {
+    const selectedDataType = dataTypeSelect.value;
+    console.log('Submitting data...');
+    // console.log(data.value);
+    // console.log('selectedDataType', selectedDataType);
+    const res = await submitFragment(user, data.value, selectedDataType);
+    console.log('>>> res', res);
+    if (!res) {
+      alert(
+        'Error! Your data was not submitted. Please make sure data type is supported'
+      );
+    }
+
+    // init();
+  };
+
+  getData.onclick = async (e) => {
+    console.log('Getting data...');
+    res = await getUserFragments(user, expandCheckbox.checked);
+
+    //remove any existing list to avoid duplicates
+    if (showFragmentList) {
+      while (showFragmentList.firstChild) {
+        showFragmentList.removeChild(showFragmentList.firstChild);
+      }
+    }
+
+    // get fragment objects from the response
+    const fragments = res.fragments;
+
+    //create a list of fragment rows
+    const fragmentRows = `${
+      fragments.length > 0
+        ? fragments
+            .map((fragment) => {
+              // let createdDate = new Date(fragment.created);
+              let fragmentId = expandCheckbox.checked ? fragment.id : fragment;
+              return `<tr>
+                  <td >
+                    <span><span class="has-text-weight-bold">ID:</span> ${fragmentId}</span><br/>
+                    ${
+                      expandCheckbox.checked
+                        ? `<span><span class="has-text-weight-bold">Created:</span> ${convertDateUS(
+                            fragment.created
+                          )}</span><br/>
+                      <span><span class="has-text-weight-bold">Type:</span> ${
+                        fragment.type
+                      }</span>`
+                        : ''
+                    }
+                  </td>
+                  <td class="is-vcentered">
+                  ${
+                    !expandCheckbox.checked
+                      ? '<em>Not Available</em>'
+                      : `<div class="select is-rounded">
+                      <select name="extension-for-${fragmentId}" id="extension-for-${fragmentId}" ${
+                          fragment.type !== 'text/markdown' ? 'disabled' : ''
+                        }>
+                        <option value="">default</option>
+                        <option value="html">.html</option>
+                        
+                      </select>
+                    </div>`
+                  }
+                  </td>
+                  <td class="is-vcentered" id=${fragmentId}>
+                    <button class="button get-detail js-modal-trigger" data-target="modal-view-data">
+                      View Data
+                    </button>
+                    <button class="button is-info is-light js-view-info-modal-trigger" data-target="modal-view-info">
+                      Info
+                    </button>
+                  </td>
+                </tr>`;
+            })
+            .join('')
+        : '<tr><td>No fragments found</td></tr>'
+    }`;
+    fragmentTableBody.innerHTML = fragmentRows;
+
+    // add the rows to the list
+    // fragmentIdList.innerHTML = fragmentRows;
+    // add list to the DOM and show it
+    showFragmentList.appendChild(fragmentTable);
+    showFragmentList.hidden = false;
+
+    // MODAL FOR VIEW DATA
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(
+      ($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', async (e) => {
+          const id = $trigger.parentNode.id;
+
+          const extSelect =
+            document.querySelector(`#extension-for-${id}`) || false;
+          let res = await getFragmentDataById(user, id, extSelect.value);
+          const fragmentDataDisplay = document.querySelector(
+            '#fragment-data-display'
+          );
+          fragmentDataDisplay.innerText = res;
+          openModal($target);
+        });
+      }
+    );
+
+    // Add a click event on various child elements to close the parent modal
+    (
+      document.querySelectorAll(
+        '.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button'
+      ) || []
+    ).forEach(($close) => {
+      const $target = $close.closest('.modal');
+
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'Escape') {
+        closeAllModals();
+      }
+    });
+
+    // MODAL FOR VIEW INFO
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-view-info-modal-trigger') || []).forEach(
+      ($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', async () => {
+          const id = $trigger.parentNode.id;
+          const data = await getFragmentById(user, id);
+          const fragment = data.fragment;
+
+          const modalCardBody = document.querySelector(
+            '#view-info-modal-card-body'
+          );
+
+          const fragmentDiv = `<div class="content">
+                                  <p><span class="has-text-weight-bold">Fragment ID: </span>${
+                                    fragment.id
+                                  }</p>
+                                  <p><span class="has-text-weight-bold">Owner ID: </span>${
+                                    fragment.ownerId
+                                  }</p>
+                                  <p><span class="has-text-weight-bold">Created: </span>${convertDateUS(
+                                    fragment.created
+                                  )}</p>
+                                  <p><span class="has-text-weight-bold">Updated: </span>${convertDateUS(
+                                    fragment.updated
+                                  )}</p>
+                                  <p><span class="has-text-weight-bold">Type: </span>${
+                                    fragment.type
+                                  }</p>
+                                  <p><span class="has-text-weight-bold">Size: </span>${
+                                    fragment.size
+                                  }</p>
+                              </div>`;
+          modalCardBody.innerHTML = fragmentDiv;
+          openModal($target);
+        });
+      }
+    );
+
+    // Add a click event on various child elements to close the parent modal
+    (
+      document.querySelectorAll(
+        '.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button'
+      ) || []
+    ).forEach(($close) => {
+      const $target = $close.closest('.modal');
+
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'Escape') {
+        closeAllModals();
+      }
+    });
+  };
 }
 
 // Wait for the DOM to be ready, then start the app
 addEventListener('DOMContentLoaded', init);
+
+// helper functions
+const convertDateUS = (dateString) => {
+  let date = new Date(dateString);
+  return date.toLocaleString('en-US');
+};
